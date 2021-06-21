84538f9ce7be6c27fa325a653dc78b5320f4215a
Vahdet Parlak <vahdetparlak@gmail.com>
2021-06-21 19:32:13 +0300

    Revert "Update: JobAdverForm page added."
    This reverts commit 940b49ae96fcc8ead4b844898f6a9b38c1d27ec1.

--- hrms-project/src/layouts/Dashboard.jsx
+++ hrms-project/src/layouts/Dashboard.jsx
@@ -6,7 +6,6 @@ import JobSeekerList from "../pages/JobSeekerList";
 import ResumeList from "../pages/ResumeList";
 import Sidebar from "./Sidebar";
 import { Grid } from "semantic-ui-react";
-import JobAdvertForm from "../pages/JobAdvertForm";
 
 export default function Dashboard() {
   return (
@@ -17,8 +16,6 @@ export default function Dashboard() {
             <Sidebar />
           </Grid.Column>
           <Grid.Column width="12">
-            <JobAdvertForm/>
-            
             <JobAdvertList />
             <ResumeList />
             <JobPositionList />
